8cb1751a3a738d4bba5fdc9bed54260acd2b07fd
Pieternauw <65928691+Pieternauw@users.noreply.github.com>
2022-01-06 11:43:51 -1000

    added game scene

--- Game V2/main.js
+++ Game V2/main.js
@@ -1,10 +1,11 @@
 import Phaser from "./src/lib/Phaser.js"
+import Game from "./src/scenes/Game.js"
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 800,
     height: 800,
-    scene: [],
+    scene: [Game],
     physics: {
         default: "arcade",
         arcade: {
