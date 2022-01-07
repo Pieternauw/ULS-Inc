5afb32dd7b723bea0389e250a5b43c5e13d8224d
Pieternauw <65928691+Pieternauw@users.noreply.github.com>
2022-01-06 15:08:32 -1000

    updated scene loading with new assets

--- Game V2/src/scenes/Dungeon.js
+++ Game V2/src/scenes/Dungeon.js
@@ -1,1 +1,20 @@
 //test file storing code for loading the assets and making the actual levels
+import Phaser from "../lib/Phaser.js"
+
+export default class Level extends Phaser.Scene {
+
+    constructor() {
+        super('level')
+    }
+
+    preload() {
+        this.load.image("floor", '../resources/dungeon/dungeon-floor.png'); //https://github.com/Pieternauw/ULS-Inc/tree/main/Game V2/src/resources/dungeon/dungeon-floor.png for testing
+        this.load.image("ground", "../resources/dungeon/dungeon-wall.png"); //https://github.com/Pieternauw/ULS-Inc/tree/main/Game V2/src/resources/dungeon/dungeon-wall.png
+        this.load.image("star", "https://labs.phaser.io/assets/demoscene/star3.png");
+        this.load.image("bomb", "https://labs.phaser.io/assets/demoscene/blue_ball.png");
+        this.load.spritesheet("dude", "https://labs.phaser.io/assets/sprites/dude.png", {
+            frameWidth: 32,
+            frameHeight: 48
+        });
+    }
+}
