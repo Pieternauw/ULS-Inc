4eee275ddbe37f4cb38ffd295087005150efc261
Pieternauw <65928691+Pieternauw@users.noreply.github.com>
2022-01-11 11:46:32 -1000

    importing the title screen

--- Game V2/src/main.js
+++ Game V2/src/main.js
@@ -1,6 +1,6 @@
 import Phaser from "https://codepen.io/nauwelaertsp2022/pen/GRMqmGY.js"
 import Game from "https://codepen.io/nauwelaertsp2022/pen/YzrjxmJ?editors=0010.js"
-import Title from ".js"
+import Title from "https://codepen.io/nauwelaertsp2022/pen/OJxaPwQ?editors=0010.js"
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
