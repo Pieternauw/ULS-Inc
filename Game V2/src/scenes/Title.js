2c0ea7d24ac8f424b02c500ab308d548ff4c47fc
Videogamelord121 <97265454+Videogamelord121@users.noreply.github.com>
2022-01-11 11:35:39 -1000

    Create Title.js

---
+++ Game V2/src/scenes/Title.js
@@ -0,0 +1,36 @@
+//import Phaser from 'https://codepen.io/bealec2022/pen/GRMoPLG.js'
+//import game file once made
+
+export default class Title extends Phaser.Scene{
+  
+  preload(){
+    this.load.image("background", "https://images.creativemarket.com/0.1.0/ps/120087/910/607/m1/fpnw/wm0/stonefloor001_large-.jpg?1401477523&s=aeb8c8fbad2e06ac22344908c9ad2c9e");   
+  }
+  
+  create(){
+    this.add.image(400,300,"background").setScale(0.5);
+    
+    keyboard = this.input.keyboard.addKeys("enter");
+    
+    var Title;
+    var Start;
+    
+    Title = this.add.text(150, 125, "Title", {
+      fontSize: "00px",
+      fill:"#000"
+    })
+    Title.setColor("White");
+    
+    Start = this.add.text(275, 200, "Hit Enter To Start Game", {
+      fontSize: "32px",
+      fill:"#000"
+    })
+    Start.setColor("White");
+  }
+  
+  update(){
+    if (keyboard.enter.isDown) {
+            //enable switch to game file
+        }
+  }
+}
