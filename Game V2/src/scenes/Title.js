d1f4c64b1275794d945270c49792f844f94e47aa
Pieternauw <65928691+Pieternauw@users.noreply.github.com>
2022-01-24 11:26:37 -1000

    Changed Style
    Updated for use with run.html

--- Game V2/src/scenes/Title.js
+++ Game V2/src/scenes/Title.js
@@ -1,36 +1,34 @@
-import Phaser from 'https://codepen.io/bealec2022/pen/GRMoPLG.js'
-//import game file once made
-
-export default class Title extends Phaser.Scene {
-
-    constructor() {
-        super({key: "title"});
-    }
-
-    preload() {
+var Title = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize: function() {
+        Phaser.Scene.call(this, { "key": "Title" });
+    },
+    //init: function() {},
+    preload: function() {
         this.load.image("background", "https://images.creativemarket.com/0.1.0/ps/120087/910/607/m1/fpnw/wm0/stonefloor001_large-.jpg?1401477523&s=aeb8c8fbad2e06ac22344908c9ad2c9e");
-    }
+    },
+    create: function() {
+        this.add.image(400, 400, "background");
 
-    create() {
-        this.add.image(400, 300, "background").setScale(0.5);
-
-        keyboard = this.input.keyboard.addKeys("enter");
-
-        var Title;
+        var title;
         var Start;
 
-        Title = this.add.text(150, 125, "Title", {
-            fontSize: "00px",
-            fill: "#000"
-        })
-        Title.setColor("White");
-
-        Start = this.add.text(275, 200, "Hit Enter To Start Game", {
+        title = this.add.text(325, 125, "Title", {
             fontSize: "32px",
             fill: "#000"
         })
+        title.setColor("White");
+
+        Start = this.add.text(275, 200, "Hit Enter To Start Game", {
+            fontSize: "16px",
+            fill: "#000"
+        })
         Start.setColor("White");
-      
-        this.input.keyboard.once('keydown-ENTER', () => { this.scene.start('game') });
+
+    },
+    update: function() {
+        this.input.keyboard.on("keydown-ENTER", function() {
+            this.scene.start("Game");
+        });
     }
-}
+});
