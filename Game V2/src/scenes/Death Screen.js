5376f824cd92a65723d5bab6f32a8bae3a693e95
Pieternauw <65928691+Pieternauw@users.noreply.github.com>
2022-01-24 11:43:44 -1000

    updated with new style
    changed style

--- Game V2/src/scenes/Death Screen.js
+++ Game V2/src/scenes/Death Screen.js
@@ -1,52 +1,46 @@
-//import Phaser from 'https://codepen.io/bealec2022/pen/GRMoPLG.js'
+var Death = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize: function() {
+        Phaser.Scene.call(this, { "key": "Death" });
+    },
+    //init: function() {},
+    preload: function() {
+        this.load.image("background", "https://images.creativemarket.com/0.1.0/ps/120087/910/607/m1/fpnw/wm0/stonefloor001_large-.jpg?1401477523&s=aeb8c8fbad2e06ac22344908c9ad2c9e");
+    },
+    create: function() {
+        this.add.image(400, 300, "background").setScale(0.5);
 
-export default class Death extends Phaser.Scene{
-  
-  constructor() {
-        super({key: "Death"});
+        keyboard = this.input.keyboard.addKeys("enter", "tab");
+
+        var deathText;
+        var restartText;
+        var titleScreenText;
+
+        deathText = this.add.text(150, 125, "Game Over", {
+            fontSize: "60px",
+            fill: "#000"
+        })
+        deathText.setColor("White");
+
+        restartText = this.add.text(275, 200, "Hit Enter To Try Again", {
+            fontSize: "32px",
+            fill: "#000"
+        })
+        restartText.setColor("White");
+
+        titleScreenText = this.add.text(325, 250, "Hit tab To Return to the Title Screen", {
+            fontSize: "32px",
+            fill: "#000"
+        })
+        titleScreenText.setColor("White");
+    },
+    update: function() {
+        if (keyboard.enter.isDown) {
+            //check code
+            this.scene.start('Game');
+        } else if (keyboard.tab.isDown) {
+            //check code
+            this.scene.start('Title');
+        }
     }
-  
-  preload(){
-    this.load.image("background", "https://images.creativemarket.com/0.1.0/ps/120087/910/607/m1/fpnw/wm0/stonefloor001_large-.jpg?1401477523&s=aeb8c8fbad2e06ac22344908c9ad2c9e");   
-  }
-  
-  create(){
-    this.add.image(400,300,"background").setScale(0.5);
-    
-    keyboard = this.input.keyboard.addKeys("enter","tab");
-    
-    var deathText;
-    var restartText;
-    var titleScreenText;
-    
-    deathText = this.add.text(150, 125, "Game Over", {
-      fontSize: "60px",
-      fill:"#000"
-    })
-    deathText.setColor("White");
-    
-    restartText = this.add.text(275, 200, "Hit Enter To Try Again", {
-      fontSize: "32px",
-      fill:"#000"
-    })
-    restartText.setColor("White");
-    
-    titleScreenText = this.add.text(325, 250, "Hit tab To Return to the Title Screen", {
-      fontSize: "32px",
-      fill:"#000"
-    })
-    titleScreenText.setColor("White");
-  }
-  
-  update(){
-    if (keyboard.enter.isDown) {
-      //check code
-      this.scene.start('game');
-    } else if (keyboard.tab.isDown){
-      //check code
-      this.scene.start('title');
-    } else {
-      retun false; 
-    }
-  }
-}
+});
