f76266c7a2f35496f460290230e14ab20b58420d
Pieternauw <65928691+Pieternauw@users.noreply.github.com>
2022-03-04 09:02:24 -1000

    updated with preload and scene call

--- Game V2/src/scenes/win-scene.js
+++ Game V2/src/scenes/win-scene.js
@@ -1,1 +1,21 @@
 //win scene
+var Win = new Phaser.Class({
+    Extends: Phaser.scene,
+    initialize: function() {
+        Phaser.Scene.call(this, { "key": "Win" });
+    },
+    init: function() {},
+    preload: function() {
+        this.load.image("floor", "https://images.creativemarket.com/0.1.0/ps/120087/910/607/m1/fpnw/wm0/stonefloor001_large-.jpg?1401477523&s=aeb8c8fbad2e06ac22344908c9ad2c9e");
+        this.load.image("ground", "https://raw.githubusercontent.com/Pieternauw/ULS-Inc/main/Game%20V2/src/resources/dungeon/dungeon-wall.png");
+        this.load.image("bomb", "https://labs.phaser.io/assets/demoscene/blue_ball.png");
+        this.load.image("enemy", "https://raw.githubusercontent.com/Pieternauw/ULS-Inc/main/Game%20V2/src/resources/sprite/skeletonminion.png");
+        this.load.spritesheet("dude", "https://raw.githubusercontent.com/Pieternauw/ULS-Inc/main/Game%20V2/src/resources/sprite/Dude.png", {
+            frameWidth: 32,
+            frameHeight: 48
+        });
+    },
+    create: function() {
+        //text + credits, maybe more stuff like the player walking past or an enemy flying by
+    }
+})
