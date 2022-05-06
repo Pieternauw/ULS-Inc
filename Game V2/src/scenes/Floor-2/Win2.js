f175420358a69e59291ffed9c9d52bd5e45f1b5f
Videogamelord121 <97265454+Videogamelord121@users.noreply.github.com>
2022-05-06 11:35:07 -1000

    Working Final Win

--- Game V2/src/scenes/Floor-2/Win2.js
+++ Game V2/src/scenes/Floor-2/Win2.js
@@ -1,57 +1,53 @@
 var Win2 = new Phaser.Class({
-    Extends: Phaser.scene,
+    Extends: Phaser.Scene,
     initialize: function() {
         Phaser.Scene.call(this, { "key": "Win2" });
     },
-    preload: function() {
-        this.load.image("winFloor", "https://raw.githubusercontent.com/Pieternauw/ULS-Inc/main/Game%20V2/src/resources/dungeon/images.jpg");
-        this.load.audio("finalWin", "https://raw.githubusercontent.com/nlaranio/CSResources/main/Jumper/assets/sfx/FINALWIN.mp3");
-    },
+    init: function() {},
     create: function() {
-      
+
         this.add.image(390, 300, "winFloor").setScale(4.7);
-      
+
         cursors = this.input.keyboard.createCursorKeys();
         keyboard = this.input.keyboard.addKeys("enter");
-      
-        var winText;
-        var winText2;
-        var continueText;
-      
-        winText = this.add.text(80, 40, "You've Conqured The Dungeon", {
-            fontSize: "40px",
-            fill: "#000"
-        })
-        winText.setColor("White");
-      
-        winText2 = this.add.text(180, 90, "Collect Your Prize", {
-            fontSize: "40px",
-            fill: "#000"
-        })
-        winText2.setColor("White");
 
-        continueText = this.add.text(150, 600, "Hit Enter To Continue", {
+        var win2Text;
+        var win2Text2;
+        var continue2Text;
+
+        win2Text = this.add.text(80, 40, "You've Conqured The Dungeon", {
             fontSize: "40px",
             fill: "#000"
         })
-        continueText.setColor("White");
-        //Audio
-        {
-          finalwinSound = this.sound.add("finalWin");
-          finalwinSound.play({
-            mute: false,
-            volume: 1,
-            rate: 1,
-            detune: 0,
-            seek: 0,
-            loop: true,
-            delay: 0.25});
-        }
+        win2Text.setColor("White");
+
+        win2Text2 = this.add.text(180, 90, "Collect Your Prize", {
+            fontSize: "40px",
+            fill: "#000"
+        })
+        win2Text2.setColor("White");
+
+        continue2Text = this.add.text(150, 600, "Hit Enter To Continue", {
+            fontSize: "40px",
+            fill: "#000"
+        })
+        continue2Text.setColor("White");
+      {
+      this.finalWinSound = this.sound.add('finalwin');
+      this.finalWinSound.play({
+          mute: false,
+          volume: 1,
+          rate: 1,
+          detune: 0,
+          seek: 0,
+          loop: true,
+          delay: 0.25});
+      } //Audio
     },
     update: function() {
         if (keyboard.enter.isDown) {
             this.scene.start("Credits");
-            finalwinSound.stop();
+            this.winnerSound.stop();
         }
     }
 });
